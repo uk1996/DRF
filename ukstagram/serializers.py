d83254d753dbe4c52b269bd7ce192dc413aa7145
uk1996 <csuk1947@gmail.com>
2022-04-21 17:58:44 +0900

    Serializer 중첩

--- ukstagram/serializers.py
+++ ukstagram/serializers.py
@@ -1,10 +1,17 @@
+from django.contrib.auth import get_user_model
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from .models import Post
 
+class AuthSerializer(ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email']
+
 class PostSerializer(ModelSerializer):
-    username = serializers.ReadOnlyField(source='author.username')
+    # username = serializers.ReadOnlyField(source='author.username')
+    author = AuthSerializer()
 
     class Meta:
         model = Post
-        fields = ['pk', 'username', 'message', 'created_at', 'updated_at']
+        fields = ['pk', 'author', 'message', 'created_at', 'updated_at']
