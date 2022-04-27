0d90d9b5019902e9855192f905bcad2e86045c46
uk1996 <csuk1947@gmail.com>
2022-04-27 15:29:03 +0900

    accounts 앱 생성, 등록

--- DRF/urls.py
+++ DRF/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('ukstagram/', include('ukstagram.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
