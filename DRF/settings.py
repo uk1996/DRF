414df211f454bf74d92b7dd9d5ba468bf5dbc843
uk1996 <csuk1947@gmail.com>
2022-04-27 11:40:19 +0900

    페이징 처리

--- DRF/settings.py
+++ DRF/settings.py
@@ -150,4 +150,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ],
+    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 }
